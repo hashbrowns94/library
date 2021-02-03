566efaaadbbf09b10a0846cebae4d8e0bab0b858
Jenn <hashbrowns120@gmail.com>
2021-02-03 14:23:33 -0600

    separated create dom elements and render dom elements functions, fixed bug where two divs were being created with each entry

--- app.js
+++ app.js
@@ -31,16 +31,6 @@ function Book(title, author, read){
     this.read = read
 }
 
-Book.prototype = {
-    addBookToLibrary(title, author, read) {
-        myLibrary.push({
-            title: title, 
-            author: author,
-            read: read});
-        console.log(myLibrary);
-    }
-}
-
 function parseForm(){
     let userTitle = document.getElementById('inputTitle').value;
     let userAuthor = document.getElementById('inputAuthor').value;
@@ -48,57 +38,68 @@ function parseForm(){
         throw new Error(alert("Please fill out all fields"));
     }
     
-    let newBook = new Book(userTitle, userAuthor);
-    newBook.addBookToLibrary(userTitle, userAuthor);
-    displayBook(newBook);
+    let newBook = new Book(userTitle, userAuthor, false);
+    //missing return?
+    //newBook.addBookToLibrary(userTitle, userAuthor);
+    let bookId = myLibrary.length + 1
+    let bookHtmlElement = createBookHtmlElement(bookId, newBook);
+    myLibrary.push(newBook);
+    console.log('bookHtmlElement', bookHtmlElement);
+    container.appendChild(bookHtmlElement);
 }
 
-function displayBook(){
-    for (i=0; i < myLibrary.length; i++){
-        const div = document.createElement('div');
-        const removeButton = document.createElement('button');
-        const titleNode = document.createTextNode(myLibrary[i].title)
-        const bylineDiv = document.createElement('div');
-        const by = document.createTextNode('by');
-        const authorNode = document.createTextNode(myLibrary[i].author);
-        const readDiv = document.createElement('div');
-        const readNode = document.createTextNode('Read');
-        const checked = document.createElement('input');
-        const slider = document.createElement('label');
-
-        div.classList.add('book');
-        bylineDiv.classList.add('byline');
-        div.id = ('book' + i)
-        console.log(div.id);
-        div.setAttribute('data-position', i);
-        readDiv.classList.add('read');
-
-        checked.type = 'checkbox';
-        checked.classList.add('readStatus');
-        checked.id = 'checkboxID'
-        slider.classList.add('switch');
-        slider.htmlFor = 'checkboxID';
-
-        
-        div.appendChild(titleNode);
-        div.appendChild(document.createElement('br'));
-        div.appendChild(bylineDiv);
-        bylineDiv.appendChild(by);
-        div.appendChild(authorNode);
-        div.appendChild(document.createElement('br'));
-        div.appendChild(readDiv)
-        readDiv.appendChild(readNode);
-        readDiv.appendChild(checked);
-        readDiv.appendChild(slider);
-        container.appendChild(div);
-
-        removeButton.setAttribute('class', 'remove');
-        removeButton.textContent = 'Remove Book';
-        div.appendChild(removeButton);
-        
+function renderAllBooks() {
+    for (i=0; i < myLibrary.length; i++){ 
+        let libraryBook = myLibrary[i];
+    
+        let bookHtmlElement = createBookHtmlElement(i + 1, libraryBook);
+        container.appendChild(bookHtmlElement);
     }
 }
-displayBook()
+
+function createBookHtmlElement(bookId, bookEntry) {
+    const div = document.createElement('div');
+    const removeButton = document.createElement('button');
+    const titleNode = document.createTextNode(bookEntry.title)
+    const bylineDiv = document.createElement('div');
+    const by = document.createTextNode('by');
+    const authorNode = document.createTextNode(bookEntry.author);
+    const readDiv = document.createElement('div');
+    const readNode = document.createTextNode('Read');
+    const checked = document.createElement('input');
+    const slider = document.createElement('label');
+    
+    div.classList.add('book');
+    bylineDiv.classList.add('byline');
+    div.id = ('book' + bookId)
+    console.log(div.id);
+    div.setAttribute('data-position', bookId);
+    readDiv.classList.add('read');
+
+    checked.type = 'checkbox';
+    checked.classList.add('readStatus');
+    checked.id = 'checkboxID' + bookId
+    slider.classList.add('switch');
+    slider.htmlFor = 'checkboxID' + bookId;
+    
+    div.appendChild(titleNode);
+    div.appendChild(document.createElement('br'));
+    div.appendChild(bylineDiv);
+    bylineDiv.appendChild(by);
+    div.appendChild(authorNode);
+    div.appendChild(document.createElement('br'));
+    div.appendChild(readDiv)
+    readDiv.appendChild(readNode);
+    readDiv.appendChild(checked);
+    readDiv.appendChild(slider);
+    
+    removeButton.setAttribute('class', 'remove');
+    removeButton.textContent = 'Remove Book';
+    div.appendChild(removeButton);
+
+    return div;
+}
+renderAllBooks();
 
 //event listeners
 add.addEventListener('click', function(){
@@ -107,7 +108,7 @@ add.addEventListener('click', function(){
 
 submit.addEventListener('click', function(){
     formDiv.style.display = 'none';
-    parseForm();
+    //parseForm();
 });
 
 function removeBook(){
